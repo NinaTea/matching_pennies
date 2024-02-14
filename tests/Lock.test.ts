4a9af52970124a28301c098ae4b65aba5186f7d5
NIna <camilagallo1700@gmail.com>
2024-02-14 09:06:05 -0300

    Translations done

--- tests/Lock.test.ts
+++ tests/Lock.test.ts
@@ -20,147 +20,112 @@ const move2  = boolCV(false);
 const nonce2  = uintCV(8254);
 const hash_2 = simnet.callReadOnlyFn("Lock", "armar_hash", [move2, nonce2], address2).result;
 
-
-
-
 /*
   The test below is an example. To learn more, read the testing documentation here:
   https://docs.hiro.so/clarinet/feature-guides/test-contract-with-clarinet-sdk
 */
 
-describe("Testing commitJugada", () => {
+describe("Testing matching_pennies", () => {
 
-   it("Jugada entera de jugadores correctos gana el primero", () => {
+   it("An entire game played, first wins", () => {
     
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata en cuenta al iniciar del address 1");
-     const response  = simnet.callPublicFn("Lock", "commitJugada", [hash_1, amount], address1);  
-     (expect(response.result)).toBeOk(stringAscii("Fue agregado exitosamente"));
-
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata después de jugar del address 1");
-
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata en cuenta al iniciar del address 2");
-     const response2  = simnet.callPublicFn("Lock", "commitJugada", [hash_2, amount], address2); 
-     (expect(response2.result)).toBeOk(stringAscii("Fue agregado exitosamente"));
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata después de jugar del address 2");
+     const response  = simnet.callPublicFn("Lock", "commit_play", [hash_1, amount], address1);  
+     (expect(response.result)).toBeOk(stringAscii("Successfully added"));
+   
+     const response2  = simnet.callPublicFn("Lock", "commit_play", [hash_2, amount], address2); 
+     (expect(response2.result)).toBeOk(stringAscii("Successfully added"));
     
-     const jugada1 = simnet.callPublicFn("Lock", "revelarJugada", [move1, nonce1], address1);
+     const jugada1 = simnet.callPublicFn("Lock", "show_my_play", [move1, nonce1], address1);
      expect(jugada1.result).toBeOk(boolCV(true));  
 
-     const resuelve = simnet.callPublicFn("Lock", "resolverJugada", [], address1);
-     expect(resuelve.result).toBeErr(stringAscii("No se puede revelar aun!"));
+     const resuelve = simnet.callPublicFn("Lock", "endPlay", [], address1);
+     expect(resuelve.result).toBeErr(stringAscii("It can not be reveal yet!"));
 
-     const jugada2 = simnet.callPublicFn("Lock", "revelarJugada", [move2, nonce2], address2);
+     const jugada2 = simnet.callPublicFn("Lock", "show_my_play", [move2, nonce2], address2);
      expect(jugada2.result).toBeOk(boolCV(true));  
 
-
-     const resuelve2 = simnet.callPublicFn("Lock", "resolverJugada", [], address2);
+     const resuelve2 = simnet.callPublicFn("Lock", "endPlay", [], address2);
      expect(resuelve2.result).toBeOk(boolCV(true));
  
-     const cobrar1 = simnet.callPublicFn("Lock", "cobrar", [principalCV(address1)], address1);
-     expect(cobrar1.result).toBeOk(stringAscii("Cobraste exitosamente"))
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata después de cobrar del address1");
+     const cobrar1 = simnet.callPublicFn("Lock", "get_prize", [principalCV(address1)], address1);
+     expect(cobrar1.result).toBeOk(stringAscii("Successfull stx-transfer"))
     
-     const cobrar2 = simnet.callPublicFn("Lock", "cobrar", [principalCV(address2)], address2);
-     expect(cobrar2.result).toBeOk(stringAscii("Cobraste exitosamente"));
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata después de cobrar del address2");
+     const cobrar2 = simnet.callPublicFn("Lock", "get_prize", [principalCV(address2)], address2);
+     expect(cobrar2.result).toBeOk(stringAscii("Successfull stx-transfer"));
    });
   
 
-   it("Jugada entera de jugadores correctos gana el segundo", () => {
+   it("An entire game played, second wins", () => {
 
     const move2  = boolCV(true);
     const nonce2  = uintCV(8254);
     const hash_2 = simnet.callReadOnlyFn("Lock", "armar_hash", [move2, nonce2], address2).result;
 
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata en cuenta al iniciar del address 1");
-     const response  = simnet.callPublicFn("Lock", "commitJugada", [hash_1, amount], address1);  
-     (expect(response.result)).toBeOk(stringAscii("Fue agregado exitosamente"));
+     const response  = simnet.callPublicFn("Lock", "commit_play", [hash_1, amount], address1);  
+     (expect(response.result)).toBeOk(stringAscii("Successfully added"));
 
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata después de jugar del address 1");
-
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata en cuenta al iniciar del address 2");
-     const response2  = simnet.callPublicFn("Lock", "commitJugada", [hash_2, amount], address2); 
-     (expect(response2.result)).toBeOk(stringAscii("Fue agregado exitosamente"));
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata después de jugar del address 2");
+     const response2  = simnet.callPublicFn("Lock", "commit_play", [hash_2, amount], address2); 
+     (expect(response2.result)).toBeOk(stringAscii("Successfully added"));
     
-     const jugada1 = simnet.callPublicFn("Lock", "revelarJugada", [move1, nonce1], address1);
+     const jugada1 = simnet.callPublicFn("Lock", "show_my_play", [move1, nonce1], address1);
      expect(jugada1.result).toBeOk(boolCV(true));  
 
-     const resuelve = simnet.callPublicFn("Lock", "resolverJugada", [], address1);
-     expect(resuelve.result).toBeErr(stringAscii("No se puede revelar aun!"));
+     const resuelve = simnet.callPublicFn("Lock", "endPlay", [], address1);
+     expect(resuelve.result).toBeErr(stringAscii("It can not be reveal yet!"));
 
-     const jugada2 = simnet.callPublicFn("Lock", "revelarJugada", [move2, nonce2], address2);
+     const jugada2 = simnet.callPublicFn("Lock", "show_my_play", [move2, nonce2], address2);
      expect(jugada2.result).toBeOk(boolCV(true));  
 
-
-     const resuelve2 = simnet.callPublicFn("Lock", "resolverJugada", [], address2);
+     const resuelve2 = simnet.callPublicFn("Lock", "endPlay", [], address2);
      expect(resuelve2.result).toBeOk(boolCV(true));
  
-     const cobrar1 = simnet.callPublicFn("Lock", "cobrar", [principalCV(address1)], address1);
-     expect(cobrar1.result).toBeOk(stringAscii("Cobraste exitosamente"))
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata después de cobrar del address1");
+     const cobrar1 = simnet.callPublicFn("Lock", "get_prize", [principalCV(address1)], address1);
+     expect(cobrar1.result).toBeOk(stringAscii("Successfull stx-transfer"))
     
-     const cobrar2 = simnet.callPublicFn("Lock", "cobrar", [principalCV(address2)], address2);
-     expect(cobrar2.result).toBeOk(stringAscii("Cobraste exitosamente"));
-     console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata después de cobrar del address2");
+     const cobrar2 = simnet.callPublicFn("Lock", "get_prize", [principalCV(address2)], address2);
+     expect(cobrar2.result).toBeOk(stringAscii("Successfull stx-transfer"));
+
   });
 
 
-  it("Jugada entera el primero hace trampa", () => {
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata en cuenta al iniciar del address 1");
-    const response  = simnet.callPublicFn("Lock", "commitJugada", [hash_1, amount], address1);  
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata después de jugar del address 1");
-
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata en cuenta al iniciar del address 2");
-    const response2  = simnet.callPublicFn("Lock", "commitJugada", [hash_2, amount], address2); 
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata después de jugar del address 2");
-
-    const jugada1 = simnet.callPublicFn("Lock", "revelarJugada", [move1, uintCV(5678)], address1);
-    expect(jugada1.result).toBeErr(stringAscii("No seas tramposo, volve a intentarlo"));
+  it("First cheats", () => {
    
-    const jugada2 = simnet.callPublicFn("Lock", "revelarJugada", [move2, nonce2], address2);
+    const response  = simnet.callPublicFn("Lock", "commit_play", [hash_1, amount], address1);  
+    
+    const response2  = simnet.callPublicFn("Lock", "commit_play", [hash_2, amount], address2); 
+
+    const jugada1 = simnet.callPublicFn("Lock", "show_my_play", [move1, uintCV(5678)], address1);
+    expect(jugada1.result).toBeErr(stringAscii("Do not cheat, try again"));
+   
+    const jugada2 = simnet.callPublicFn("Lock", "show_my_play", [move2, nonce2], address2);
     expect(jugada2.result).toBeOk(boolCV(true));
    
   });
 
-  it("Jugada entera el segundo hace trampa", () => {
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata en cuenta al iniciar del address 1");
-    const response  = simnet.callPublicFn("Lock", "commitJugada", [hash_1, amount], address1);  
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata después de jugar del address 1");
+  it("Second cheats", () => {
+   
+    const response  = simnet.callPublicFn("Lock", "commit_play", [hash_1, amount], address1);  
+    const response2  = simnet.callPublicFn("Lock", "commit_play", [hash_2, amount], address2); 
 
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata en cuenta al iniciar del address 2");
-    const response2  = simnet.callPublicFn("Lock", "commitJugada", [hash_2, amount], address2); 
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata después de jugar del address 2");
-
-    const jugada1 = simnet.callPublicFn("Lock", "revelarJugada", [move1, nonce1], address1);
+    const jugada1 = simnet.callPublicFn("Lock", "show_my_play", [move1, nonce1], address1);
     expect(jugada1.result).toBeOk(boolCV(true));
    
-    const jugada2 = simnet.callPublicFn("Lock", "revelarJugada", [move2, uintCV(1234)], address2);
-    expect(jugada2.result).toBeErr(stringAscii("No seas tramposo, volve a intentarlo"));
+    const jugada2 = simnet.callPublicFn("Lock", "show_my_play", [move2, uintCV(1234)], address2);
+    expect(jugada2.result).toBeErr(stringAscii("Do not cheat, try again"));
    
   });
 
-  it("Un tercero quiere jugar", () => {
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata en cuenta al iniciar del address 1");
-    const response  = simnet.callPublicFn("Lock", "commitJugada", [hash_1, amount], address1);  
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address1).result, "Plata después de jugar del address 1");
-
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata en cuenta al iniciar del address 2");
-    const response2  = simnet.callPublicFn("Lock", "commitJugada", [hash_2, amount], address2); 
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address2).result, "Plata después de jugar del address 2");
-
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address4).result, "Plata en cuenta al iniciar del address 4");
-    const response4  = simnet.callPublicFn("Lock", "commitJugada", [hash_2, amount], address4); 
-    expect(response4.result).toBeErr(stringAscii("Ya hay 2 jugadores"));
-    console.log(simnet.callReadOnlyFn("Lock", "balance", [], address4).result, "Plata después de jugar del address 4");
-
-    // const jugada1 = simnet.callPublicFn("Lock", "revelarJugada", [move1, nonce1], address1);
-    // expect(jugada1.result).toBeOk(boolCV(true));
+  it("A third player wants to play", () => {
    
-    // const jugada2 = simnet.callPublicFn("Lock", "revelarJugada", [move2, uintCV(1234)], address2);
-    // expect(jugada2.result).toBeErr(stringAscii("No seas tramposo, volve a intentarlo"));
+    const response  = simnet.callPublicFn("Lock", "commit_play", [hash_1, amount], address1);  
+    const response2  = simnet.callPublicFn("Lock", "commit_play", [hash_2, amount], address2); 
+    const response4  = simnet.callPublicFn("Lock", "commit_play", [hash_2, amount], address4); 
+    expect(response4.result).toBeErr(stringAscii("There are already two players!"));
    
   });
 
-
+  it("Someone who did not play wants to get the prize", () => {
+    const jugada2 = simnet.callPublicFn("Lock", "get_prize", [principalCV(address2)], address2);
+    expect(jugada2.result).toBeErr(stringAscii("You did not play"));
+  });
 });
